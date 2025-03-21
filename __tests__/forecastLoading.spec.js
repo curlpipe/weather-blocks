ad483e224398af40e0f179b6629d42b49f168464
Luke <11898833+curlpipe@users.noreply.github.com>
2025-03-21 16:15:49 +0000

    Implemented basic skeleton for tests

---
+++ __tests__/forecastLoading.spec.js
@@ -0,0 +1,9 @@
+describe("forecasts should be loaded", () => {
+  test("forecasts should load initial values", () => {
+
+  });
+  test("forecasts should update after being added", () => {
+
+  });
+  
+});
