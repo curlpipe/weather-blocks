3a2e1a155ae750bd48070d2e2c348244f4ccd64b
Luke <11898833+curlpipe@users.noreply.github.com>
2025-03-21 16:47:37 +0000

    Updated specification

--- __tests__/forecastLoading.spec.js
+++
@@ -1,9 +0,0 @@
-describe("forecasts should be loaded", () => {
-  test("forecasts should load initial values", () => {
-
-  });
-  test("forecasts should update after being added", () => {
-
-  });
-  
-});
